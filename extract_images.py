ecea486991a7f72479bd58c5d966473d26c63c8d
Frederik De Bleser <frederik@debleser.be>
2024-08-23 10:57:38 +0200

    Add a script to extract real/control images from combined image

---
+++ extract_images.py
@@ -0,0 +1,25 @@
+import argparse
+from PIL import Image
+
+
+def extract_images(source_image_path, output_image_path_prefix):
+    img = Image.open(source_image_path)
+    real_image = img.crop((0, 0, 512, 512))
+    real_image_path = f"{output_image_path_prefix}_real.jpg"
+    real_image.save(real_image_path)
+    control_image_path = f"{output_image_path_prefix}_control.jpg"
+    control_image = img.crop((512, 0, 1024, 512))
+    control_image.save(control_image_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "Extract real and control images from a source image"
+    )
+    parser.add_argument("source_image_path", type=str, help="Path to source image")
+    parser.add_argument(
+        "output_image_path_prefix", type=str, help="Prefix for output image paths"
+    )
+    args = parser.parse_args()
+
+    extract_images(args.source_image_path, args.output_image_path_prefix)
